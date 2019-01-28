4345f8545bf4f95f1b16661198a2abc225be922d
Mina <mina.naghshnejad@gmail.com>
2019-01-28 12:57:25 -0800

    code correction, removing first text row to be able to convert columns to numeric

--- tableRead.py
+++ tableRead.py
@@ -9,7 +9,7 @@ df['first']=fist[0]
 df['second']=fist[1]
 df['second']=df['second'].str.strip()
 df2=df['first','second']
-
+df2=df2.iloc[1:,:]
 
 df2['first']=pd.to_numeric(df2['first'],errors='coerce')
 df2['second']=pd.to_numeric(df2['second'],errors='coerce')
